c02e4ce8c022526a677d99e42f88ec586992e118
Gregory <gregorybridgesarl@gmail.com>
2026-09-09 01:51:55 +0000

    Sep 8, 2026, 9:51 PM

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
 # https://flask.palletsprojects.com/en/stable/quickstart/
 # Reference for starting example
 # https://www.sqlitetutorial.net/sqlite-python/creating-database/ SQLite tut
+#https://medium.com/@icodewithben/flask-sqlite-login-and-register-form-48640743bf55
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
 
 app = Flask(__name__, template_folder="../frontend/public")
-
+app.secret_key = "7H6dJd0DKDd-gD6h2KD"
+DATABASE = "database.db"
 
 @app.route("/")
 def hello_world():

f5b2c922431e57f7fe5bfe1de9509d7e44806ac5
Gregory <gregorybridgesarl@gmail.com>
2026-09-09 01:45:39 +0000

    Sep 8, 2026, 9:45 PM

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,12 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="frontend/public")
 
 
 @app.route("/")
 def hello_world():
-    return render_template("./frontend/public/login.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

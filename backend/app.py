a023e17e57922ba8a74367806902f4f6ad7b79bc
Gregory <gregorybridgesarl@gmail.com>
2026-09-09 01:40:01 +0000

    Sep 8, 2026, 9:40 PM

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+# https://flask.palletsprojects.com/en/stable/quickstart/
+# Reference for starting example
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)

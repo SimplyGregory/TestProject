e1abf32f864b2dac037a688a52cbc46e08fc6dd0
Gregory <gregorybridgesarl@gmail.com>
2026-09-09 01:42:38 +0000

    Sep 8, 2026, 9:42 PM

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("login.html")
+    return render_template("./frontend/public/login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
